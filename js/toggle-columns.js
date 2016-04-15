4cb9e65c149de974057b792626dadba3f47e81ff
LaVillaine <LaVillaine@users.noreply.github.com>
2016-04-15 12:56:10 +0100

    hide comments

--- js/toggle-columns.js
+++ js/toggle-columns.js
@@ -1,7 +1,3 @@
-/*$('[data-toggle=offcanvas]').click(function() {
-  $('.row-offcanvas-left').toggleClass('active');
-});*/
-
 $('[data-toggle=showMe]').click(function() {
   $('.hide-me').toggleClass('active');
 });
